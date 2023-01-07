d62a835a33518840042c76ee20983688ae6178c1
Tahazzot <iamtahazzot@gmail.com>
2023-01-07 20:37:59 +0600

    [update] sing-up-form app

--- projects/react/sign-up-form/src/App.tsx
+++ projects/react/sign-up-form/src/App.tsx
@@ -1,11 +1,10 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div>
-      welcome to react
+      welcome to reacts
     </div>
   );
 }
